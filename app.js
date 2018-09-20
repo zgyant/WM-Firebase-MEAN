421313f51b3e149fd2d761e2225bf560f79b925a
Jayant <z9yant@gmail.com>
2018-09-20 13:21:41 +1000

    Improvements
    Added Clusters
    3 Level users view

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+
+
 
 var app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +18,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public/dashboard')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/users', function(req, res, next) {
+    res.sendfile('public/dashboard/users.html');
+});
+
+router.get('/mod', function(req, res, next) {
+    res.sendfile('public/dashboard/moderator.html');
+});
+
+
 module.exports = router;
