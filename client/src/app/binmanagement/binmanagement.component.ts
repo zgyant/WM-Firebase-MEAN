57b03b29d0961f257578f1e7953a642a42c2bb31
Jayant <z9yant@gmail.com>
2018-10-04 16:29:43 +1000

    Revert "Revert "angular implementation (pull request #5)""
    This reverts commit 3c8779379e2231141965592fc4d6f51db4072367.

---
+++ client/src/app/binmanagement/binmanagement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-binmanagement',
+  templateUrl: './binmanagement.component.html',
+  styleUrls: ['./binmanagement.component.css']
+})
+export class BinmanagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
