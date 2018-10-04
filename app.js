3c8779379e2231141965592fc4d6f51db4072367
z9yant NA <z9yant@gmail.com>
2018-10-04 06:07:44 +0000

    Revert "angular implementation (pull request #5)"
    This reverts pull request #5.
    
    > angular implementation

--- app.js
+++ app.js
@@ -10,18 +10,14 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-//middleware
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/dist/wastemanagement')));
-app.use('/', indexRouter);
+app.use(express.static(path.join(__dirname, 'public/dashboard')));
 
-//catch other router and send response
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'client/dist/wastemanagement/index.html'))
-});
+app.use('/', indexRouter);
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  res.sendFile('../client/dist/wastemanagement/index.html');
+  res.render('index');
 });
+
+router.get('/users', function(req, res, next) {
+    res.sendfile('public/dashboard/users.html');
+});
+
+router.get('/mod', function(req, res, next) {
+    res.sendfile('public/dashboard/moderator.html');
+});
+
 
 module.exports = router;
